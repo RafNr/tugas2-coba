fc2f4fe6d1b9b0a8211e1f3365ba1ff73762b1ce
rafi nur <muhammad.rafi14@ui.ac.id>
2022-09-22 21:48:44 +0700

    initial commit

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('wishlist.urls')),
-    path('', include('example_app.urls')),
+    path('', include('wishlist.urls')),
     path('wishlist/', include('wishlist.urls')),
 
 ]
